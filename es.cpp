0281eceea6a2afd9fc5c5dba5fbec27becb37402
Jakub Stejskal <xstejs24@stud.fit.vutbr.cz>
2017-03-22 00:06:38 +0100

    Funkční, občas nastává deadlock, vyřešit

--- es.cpp
+++ es.cpp
@@ -17,15 +17,17 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
- 
-#define BUFSIZE 8
+#include <string> 
+
+#define BUFSIZE 1
 #define TAG 0
 #define SIZE 1
 
 #define TAG_BUS 1
 #define TAG_LIN 2
 #define TAG_SOR 3
-#define TAG_DON 4
+#define TAG_DON_VAL 4
+#define TAG_DON_IND 5
 #define EXEC_PROC 0
 
 using namespace std;
@@ -35,6 +37,10 @@ typedef struct {
 	int procId;
 	int nextProc;
 	int prevProc;
+	int regC;
+	int regX;
+	int regY;
+	int regZ;
 } T_SORT;
 
 
@@ -54,11 +60,11 @@ void executiveSort(T_SORT* sort){
 	int number;
 	vector<int>::iterator i;
 	vector<int> numbers;
-	vector<int> sortedNumbers;
+	int sortedNumbers[sort->procCount-1];
 	int numberX;
 	int indexX;
 	
-	cerr<<"Procesor: "<<sort->nextProc<<endl;
+//	cerr<<"Procesor: "<<sort->nextProc<<endl;
 	
 	while(1){
 		number = inputFile.get();
@@ -82,104 +88,94 @@ void executiveSort(T_SORT* sort){
 //	MPI_Bcast(&numCnt,1,MPI_INT,0,MPI_COMM_WORLD);
 	
 	// Rozesílání čísel
-	for(int i = 0; i < (sort->procCount-1); i++)
+	for(int i = 0; i < numCnt; i++)
 	{
 		numberX = numbers[i];
 		indexX = i;
-		cerr<<"numberX: "<<numberX<<"| indexX: "<<indexX<<endl;
+//		cerr<<"numberX: "<<numberX<<"| indexX: "<<indexX<<" to processor: "<<i+1<<endl;
 		// Sběrnice
-		MPI_Send(&numberX, BUFSIZE, MPI_INTEGER, i+1, TAG_BUS, MPI_COMM_WORLD);
-		MPI_Send(&indexX, BUFSIZE, MPI_INTEGER, i+1, TAG_BUS, MPI_COMM_WORLD);
+		MPI_Send(&numberX, BUFSIZE, MPI_INT, i+1, TAG_BUS, MPI_COMM_WORLD);
+//		MPI_Send(&indexX, BUFSIZE, MPI_INT, i+1, TAG_BUS, MPI_COMM_WORLD);
 		
 		// Lineární spojení
-		MPI_Send(&numberX, BUFSIZE, MPI_INTEGER, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);
-		MPI_Send(&indexX, BUFSIZE, MPI_INTEGER, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);
+		MPI_Send(&numberX, BUFSIZE, MPI_INT, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);
+//		MPI_Send(&indexX, BUFSIZE, MPI_INT, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);		
 	}
 	
+	int recv[2];
 	
 	// Přijmutí seřazených čísel
-	for(int i = 0; i < (sort->procCount-1); i++)
+	for(int i = 0; i < numCnt; i++)
 	{
-		cerr<<"Procesor "<<EXEC_PROC<<" přijímá seřazená čísla..."<<endl;
-		MPI_Recv(&numberX, BUFSIZE, MPI_INTEGER, MPI_ANY_SOURCE, TAG_DON, MPI_COMM_WORLD, &stat);
-		MPI_Recv(&indexX, BUFSIZE, MPI_INTEGER, MPI_ANY_SOURCE, TAG_DON, MPI_COMM_WORLD, &stat);
-		cerr<<"přijmuto"<<endl;
-		sortedNumbers[indexX] = numberX;
+		MPI_Recv(&recv, 2, MPI_INT, MPI_ANY_SOURCE, TAG_DON_VAL, MPI_COMM_WORLD, &stat);
+//		MPI_Recv(&sortedIndex, BUFSIZE, MPI_INT, MPI_ANY_SOURCE, TAG_DON_IND, MPI_COMM_WORLD, &stat);
+		
+//		cerr<<"Sorted: Num:"<<sortedNumber[0]<<" index:"<<sortedNumber[1]<<endl;
+		sortedNumbers[recv[1]-1] = recv[0];
 	}
+//	cerr<<"Přijmuto"<<endl;
 	
-	for(i = sortedNumbers.begin(); i != sortedNumbers.end(); ++i)
-		cout<<*i<<"\n";
-	cout<<endl;
-	
-	
+	// Výsledný výpis
+	for(int z = 0; z < (sort->procCount-1); z++)
+		cout<<sortedNumbers[z]<<endl;	
 }
 
 void sortNumbers(T_SORT* sort){
-	int X;
-	int indexX;
-	int Y;
-	int indexY;
-	int C  = 1;
-//	cerr<<"C:"<<C<<endl;
-	int Z;
-	int indexZ = sort->procId;
-	sort->nextProc = sort->procId+1;
-	sort->prevProc = sort->procId-1;
-	
-	cerr<<"Procesor: "<<sort->nextProc<<endl;
+	int indexZ;
 	
 	// Nahrání hodnoty X a jeho indexu ze sběrnice
-	MPI_Recv(&X, BUFSIZE, MPI_INTEGER, EXEC_PROC, TAG_BUS, MPI_COMM_WORLD, &stat);
-	MPI_Recv(&indexX, BUFSIZE, MPI_INTEGER, EXEC_PROC, TAG_BUS, MPI_COMM_WORLD, &stat);
-	
-	cerr<<"X: "<<X<<endl;
+	MPI_Recv(&sort->regX, BUFSIZE, MPI_INT, EXEC_PROC, TAG_BUS, MPI_COMM_WORLD, &stat);
+//	MPI_Recv(&indexX, BUFSIZE, MPI_INT, EXEC_PROC, TAG_BUS, MPI_COMM_WORLD, &stat);
 	
 	// Přijmutí seřazených čísel
 	for(int i = 0; i < (sort->procCount-1); i++)
 	{
 		// Lineární spojení - získání hodnot
-		MPI_Recv(&Y, BUFSIZE, MPI_INTEGER, sort->prevProc, TAG_LIN, MPI_COMM_WORLD, &stat);
-		MPI_Recv(&indexY, BUFSIZE, MPI_INTEGER, sort->prevProc, TAG_LIN, MPI_COMM_WORLD, &stat);
-		
-		cerr<<"Y: "<<Y<<endl;
+		MPI_Recv(&sort->regY, BUFSIZE, MPI_INT, sort->prevProc, TAG_LIN, MPI_COMM_WORLD, &stat);
+//		MPI_Recv(&indexY, BUFSIZE, MPI_INT, sort->prevProc, TAG_LIN, MPI_COMM_WORLD, &stat);			
 		
 		// Každý procesor se neprázdným X a Y porovná X a Y
 		// pokud X > Y -> C++
-//		if (X > Y)
-//			C++;
-		//C = X > Y ? C+1 : C;
-		
-		cerr<<"C:"<<C<<" P: "<<sort->procId<<endl;
+		if (sort->regX > sort->regY)
+			sort->regC++;
 		
 		// Lineární spojení - posun Y vpravo
 		if(sort->nextProc < sort->procCount)
 		{
-			MPI_Send(&Y, BUFSIZE, MPI_INTEGER, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);
-			MPI_Send(&indexY, BUFSIZE, MPI_INTEGER, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);			
+			MPI_Send(&sort->regY, BUFSIZE, MPI_INT, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);
+//			MPI_Send(&indexY, BUFSIZE, MPI_INT, sort->nextProc, TAG_LIN, MPI_COMM_WORLD);			
 		}
-
 	}
 	
-	cerr<<"test"<<endl;
+//	cerr<<"Procesor: "<<sort->procId<<" Hodnota X: "<<sort->regX<<" Správný index: "<<sort->regC<<endl;
 	
 	// Poslání hodnoty X procesoru s ID == C (Sběrnice)
-	MPI_Send(&X, BUFSIZE, MPI_INTEGER, C, TAG_SOR, MPI_COMM_WORLD);
-	MPI_Send(&indexX, BUFSIZE, MPI_INTEGER, C, TAG_SOR, MPI_COMM_WORLD);
-	// Tady ještě OK
+	MPI_Send(&sort->regX, BUFSIZE, MPI_INT, sort->regC, TAG_SOR, MPI_COMM_WORLD);
+//	MPI_Send(&(sort->regC), BUFSIZE, MPI_INT, sort->regC, TAG_SOR, MPI_COMM_WORLD);
 	
+	// Uložení posladné hodnoty X do registru Z (sorted)	
+	MPI_Recv(&sort->regZ, BUFSIZE, MPI_INT, MPI_ANY_SOURCE, TAG_SOR, MPI_COMM_WORLD, &stat);
+//	MPI_Recv(&indexZ, BUFSIZE, MPI_INT, MPI_ANY_SOURCE, TAG_SOR, MPI_COMM_WORLD, &stat);
 	
-	// Uložení posladné hodnoty X do registru Z (sorted)	 !!!TADY TO NEPROJDE!!!
-	MPI_Recv(&Z, BUFSIZE, MPI_INTEGER, MPI_ANY_SOURCE, TAG_SOR, MPI_COMM_WORLD, &stat);
-	MPI_Recv(&indexZ, BUFSIZE, MPI_INTEGER, MPI_ANY_SOURCE, TAG_SOR, MPI_COMM_WORLD, &stat);
+//	cerr<<"Procesor: "<<sort->procId<<" Hodnota Z: "<<sort->regZ<<endl;
 	
-	cerr<<"Funguje"<<endl;
+	int send[2];
+	send[0] = sort->regZ;
+	send[1] = sort->procId;
 	
 	// Odeslání čísla a jeho správného indexu řídícímu procesoru
-	MPI_Send(&Z, BUFSIZE, MPI_INTEGER, EXEC_PROC, TAG_DON, MPI_COMM_WORLD);
-	MPI_Send(&indexZ, BUFSIZE, MPI_INTEGER, EXEC_PROC, TAG_DON, MPI_COMM_WORLD);
+	MPI_Send(&send, 2, MPI_INT, EXEC_PROC, TAG_DON_VAL, MPI_COMM_WORLD);
 	
+//	cerr<<"Ukončuji procesor: "<<sort->procId<<endl;
+//	
+//	indexZ = sort->procId;
+//	cerr<<"Procesor: "<<sort->procId<<" Zindex před odesláním: "<<indexZ<<endl;
+////	
+//	MPI_Send(&indexZ, BUFSIZE, MPI_INTEGER, EXEC_PROC, TAG_DON_IND, MPI_COMM_WORLD);
+//	cerr<<"Procesor: "<<sort->procId<<" Odesilá hodnotu a správný index procesoru 0"<<endl;
 }
 
+// Měření - MPI_wtime
 
  int main(int argc, char *argv[])
  {
@@ -193,46 +189,38 @@ void sortNumbers(T_SORT* sort){
 	int myid;
 	int i;*/
 	MPI_Status stat; 
+	double time1, time2;
+	bool analyzis = false;
  
 	MPI_Init(&argc,&argv); /* inicializace MPI */
 	MPI_Comm_size(MPI_COMM_WORLD,&sort->procCount); /* zjistĂ­me, kolik procesĹŻ bÄ›ĹľĂ­ */
 	MPI_Comm_rank(MPI_COMM_WORLD,&sort->procId); /* zjistĂ­me id svĂ©ho procesu */
    
+	sort->regC = 1;
+	sort->nextProc = sort->procId+1;
+	sort->prevProc = sort->procId-1;
+
+	// Začátek spuštění procesoru
+	time1 = MPI_Wtime();
+	
 	if(sort->procId == 0)
 		executiveSort(sort);
 	else
 		sortNumbers(sort);
 	
+	// Konec spuštění procesoru
+	time2 = MPI_Wtime();
 	
+	// Výpis času
+	if(analyzis)
+	{
+		printf("MPI_Wtime() procesoru %d: %1.6f\n",sort->procId, time2-time1);
+		fflush(stdout);		
+	}
+	
+	free(sort);
 	MPI_Finalize(); 
 	return EXIT_SUCCESS;
  
-   /* ProtoĹľe vĹˇechny programy majĂ­ stejnĂ˝ kĂłd (Same Program, Multiple Data â€“ SPMD) rozdÄ›lĂ­me ÄŤinnost
-      programĹŻ podle jejich ranku. Program s rankem 0 rozeĹˇle postupnÄ› vĹˇem zprĂˇvu a pĹ™ijme od vĹˇech
-       odpovÄ›ÄŹ 
-   */
-   /*if(myid == 0)
-   {
-     printf("%d: We have %d processors\n", myid, numprocs);
-     for(i=1;i<numprocs;i++)
-     {
-       sprintf(buff, "Hello %d! ", i);
-       MPI_Send(buff, BUFSIZE, MPI_INTEGER, i, TAG, MPI_COMM_WORLD);
-     }
-     for(i=1;i<numprocs;i++)
-     {
-       MPI_Recv(buff, BUFSIZE, MPI_INTEGER, i, TAG, MPI_COMM_WORLD, &stat);
-       printf("%d: %s\n", myid, buff);
-     }
-   }
-   else
-   {
-     MPI_Recv(buff, BUFSIZE, MPI_INTEGER, 0, TAG, MPI_COMM_WORLD, &stat);
-     sprintf(idstr, "Processor %d ", myid);
-     strcat(buff, idstr);
-     strcat(buff, "OK\n");
-     MPI_Send(buff, BUFSIZE, MPI_INTEGER, 0, TAG, MPI_COMM_WORLD);
-   }
-   return 0;*/
  }
 
